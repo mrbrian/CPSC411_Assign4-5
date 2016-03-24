7a9aa6fb63a390635cd9319067e8b58103d717b3
bgyee <bgyee@ucalgary.ca>
2016-03-23 18:38:42 -0600

    wip

--- a4/AST.hs
+++ a4/AST.hs
@@ -1,28 +1,28 @@
 module AST where
 
-data M_prog a = M_prog ([M_decl a],[M_stmt a])
+data M_prog = M_prog ([M_decl],[M_stmt])
            deriving (Eq,Show,Read)
-data M_decl a = M_var (String,[M_expr a],M_type a)
-            | M_fun (String,[(String,Int,M_type a)], M_type a,[M_decl a],[M_stmt a])
+data M_decl = M_var (String,[M_expr],M_type)
+            | M_fun (String,[(String,Int,M_type)], M_type,[M_decl],[M_stmt])
            deriving (Eq,Show,Read)
-data M_stmt a = M_ass (String,[M_expr a], M_expr a)
-            | M_while (M_expr a, M_stmt a)
-            | M_cond (M_expr a, M_stmt a,M_stmt a) 
-            | M_read (String, [M_expr a])
-            | M_print (M_expr a)
-            | M_return (M_expr a)
-            | M_block ([M_decl a],[M_stmt a])
+data M_stmt = M_ass (String,[M_expr], M_expr)
+            | M_while (M_expr, M_stmt)
+            | M_cond (M_expr, M_stmt,M_stmt) 
+            | M_read (String, [M_expr])
+            | M_print (M_expr)
+            | M_return (M_expr)
+            | M_block ([M_decl],[M_stmt])
            deriving (Eq,Show,Read)
-data M_type a = M_int | M_bool | M_real 
+data M_type = M_int | M_bool | M_real 
            deriving (Eq,Show,Read)
-data M_expr a = M_ival Integer
+data M_expr = M_ival Integer
             | M_rval Float
             | M_bval Bool
             | M_size (String,Int)
-            | M_id (String,[M_expr a])
-            | M_app (M_operation a,[M_expr a])
+            | M_id (String,[M_expr])
+            | M_app (M_operation,[M_expr])
            deriving (Eq,Show,Read)
-data M_operation a = M_fn String | M_add | M_mul | M_sub | M_div | M_neg
+data M_operation = M_fn String | M_add | M_mul | M_sub | M_div | M_neg
                  | M_lt | M_le | M_gt | M_ge | M_eq | M_not | M_and | M_or
                  | M_float | M_floor | M_ceil
            deriving (Eq,Show,Read)
--- a4/SymbolTable.hs
+++ a4/SymbolTable.hs
@@ -2,13 +2,12 @@ module SymbolTable where --(ST,empty,new_scope,insert,lookup,return)
 
 import AST
 
-
 data SYM_DESC = ARGUMENT (String,M_type,Int)
               | VARIABLE (String,M_type,Int)
               | FUNCTION (String,[(M_type,Int)],M_type)
               | DATATYPE String 
               | CONSTRUCTOR (String, [M_type], String)
-
+              deriving (Eq, Show)
 -- I_VARIABLE (level, offset, type, dimension)
 -- I_FUNCTION (level, label, [arguments], return type)
 -- I_CONSTRUCTOR (int, [type], datatype name)
@@ -16,17 +15,22 @@ data SYM_DESC = ARGUMENT (String,M_type,Int)
 data SYM_I_DESC = I_VARIABLE (Int,Int,M_type,Int)
                     | I_FUNCTION (Int,String,[(M_type,Int)],M_type)
                     | I_CONSTRUCTOR (Int,[M_type],String)
-                    | I_TYPE [String];
+                    | I_TYPE [String]
+                    deriving (Eq, Show)
 
-data ScopeType = L_PROG | L_FUN M_type | L_BLK | L_CASE
+data ScopeType = L_PROG | L_FUN M_type | L_BLK | L_CASE 
+                 deriving (Eq, Show)
 
+--Var_attr (offset, M_type, dim)               
 data SYM_VALUE = Var_attr (Int,M_type,Int)
                   | Fun_attr (String,[(M_type,Int)],M_type)
                   | Con_attr (Int, [M_type], String)
                   | Typ_attr [String]
+                  deriving (Eq, Show)
 
 -- Symbol_table (leve, offset, [(String,SYM_VALUE)])
 data SYM_TABLE = Symbol_table (Int,Int,[(String,SYM_VALUE)])
+                 deriving (Eq, Show)
 
 type ST = [SYM_TABLE]
 
@@ -34,7 +38,7 @@ empty:: ST
 empty = []
 
 new_scope :: ScopeType -> ST -> ST
-new_scope s = (Symbol_table(0,0,[])):s
+new_scope t s = (Symbol_table(0,0,[])) : s
 
 lookup :: ST -> String -> SYM_I_DESC 
 lookup s x = find 0 s 
@@ -54,31 +58,34 @@ lookup s x = find 0 s
 	          Just v -> found n v         -- return as an IVAR or IFUN.
 		  Nothing -> find (n+1) rest) -- look at next ST.
 
+-- so.  its not a new scope!   so ok yes  inserting a var symbol or something.
 insert :: Int -> ST -> SYM_DESC -> (Int,ST) 
-insert n [] d =  error "Symbol table error: insertion before defining scope."
-insert n ((Symbol_table(nL,nA,sL)):rest) (ARGUMENT(str,t,dim)) 
-           | (in_index_list str sL) = error
-                ("Symbol table error: " ++ str ++"is already defined.")
-           | otherwise = (n,Symbol_table(nL,nA+1
-                             ,(str,Var_attr(~(nA+4),t,dim))::sL))	-- what is ~(nA+4)?
-	where in_index_list str [] = False
-              in_index_list str ((x,_):xs)| str==x = True
-                                          | otherwise = in_index_list str xs
-insert n ((Symbol_table(nL,nA,sL)):rest) (VARIABLE (str,t,dim)) 
-           | (in_index_list str sL) 
-	       = error ("Symbol table error: "++ str ++"is already defined.")
-           | otherwise = (n,Symbol_table(nL+1,nA
-                             ,(str,Var_attr(nL+1,t,dim))::sL))
-	where in_index_list str [] = False
-              in_index_list str ((x,_):xs)| str==x = True
-                                          | otherwise = in_index_list str xs
-insert n ((Symbol_table(nL,nA,sL)):rest) FUNCTION (str,ts,t)
-           | in_index_list str sL 
-	       = error ("Symbol table error: "++str++"is already defined.")
-           | otherwise = (n+1,(Symbol_table(nL,nA,(str,Fun_attr(getlabel n "fn",ts,t)):sL)
-                              ):rest)
-	where in_index_list str [] = False
-              in_index_list str ((x,_):xs)| str==x = True
-                                          | otherwise = in_index_list str xs
+insert n [] d = error "Symbol table error: insertion before defining scope."
 
+-- nL = level, nA = numArgs, str = ?, sL = symbolList?
+insert n ((Symbol_table(nL,nA,sL)):rest) (ARGUMENT(str,t,dim)) 
+	   | (in_index_list str sL) = error ("Symbol table error: " ++ str ++"is already defined.")
+	   | otherwise = (n, Symbol_table(nL,nA+1,(str,Var_attr(nA+4,t,dim)) : sL) : rest)	-- what is ~(nA+4)?  offset..  why + 4?  am I inserting a variable??  why nA + 1
+
+insert n ((Symbol_table(nL,nA,sL)):rest) (VARIABLE (str,t,dim)) 
+	   | (in_index_list str sL) = error ("Symbol table error: "++ str ++"is already defined.")
+	   | otherwise = (n,Symbol_table(nL+1,nA,(str,Var_attr(nL+1,t,dim)):sL) : rest)
+	   
+insert n ((Symbol_table(nL,nA,sL)):rest) (FUNCTION (str,ts,t))
+	   | in_index_list str sL = error ("Symbol table error: "++str++"is already defined.")
+	   | otherwise = (n+1,(Symbol_table(nL,nA,(str,Fun_attr(getlabel n "fn",ts,t)):sL)):rest)
+
+in_index_list :: String -> [(String, SYM_VALUE)] -> Bool
+in_index_list str [] = False
+in_index_list str ((x,_):xs)
+	  | str == x = True
+	  | otherwise = in_index_list str xs
+
+getlabel :: Int -> String -> String
+getlabel n str = ""
+
+--	  | 
 --return:: ST -> M_type
+
+--process :: M_prog -> ST
+-- every M_decl node 
