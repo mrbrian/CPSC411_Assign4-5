15c7e3ce48d9241b3437c78a7507b43513218c33
bgyee <bgyee@ucalgary.ca>
2016-04-05 16:25:18 -0600

    wip

--- IR.hs
+++ IR.hs
@@ -52,8 +52,6 @@ data I_expr = IINT      Int
 data I_opn = ICALL      (String,Int)
            | IADD_F | IMUL_F | ISUB_F | IDIV_F | INEG_F
            | ILT_F  | ILE_F  | IGT_F  | IGE_F  | IEQ_F   -- operations for floats
-           | IADD_I | IMUL_I | ISUB_I | IDIV_I | INEG_I
-           | ILT_I  | ILE_I  | IGT_I  | IGE_I  | IEQ_I   -- operations for floats
            | IADD | IMUL | ISUB | IDIV | INEG
            | ILT  | ILE  | IGT  | IGE  | IEQ 
            | INOT | IAND | IOR | IFLOAT | ICEIL |IFLOOR
@@ -88,32 +86,130 @@ transId (M_id (name, arr_exps)) st = typ
 	where
 		I_VARIABLE (lvl, off, typ, dim) = look_up st name
 
-		
+get_type_sid :: SYM_I_DESC -> M_type
+get_type_sid sym = case sym of
+	I_VARIABLE (_,_,t,_) -> t
+	I_FUNCTION (_,_,_,t) -> t
+
+get_type_expr :: ST -> M_expr -> M_type
+get_type_expr st exp = case exp of
+			M_ival _ -> M_int
+			M_rval _ -> M_real
+			M_bval _ -> M_bool
+			M_size _ -> M_int
+			M_id (str,exs) -> type'
+				where 
+					desc = look_up st str
+					type' = get_type_sid desc
+			M_app (_, e:exs) -> get_type_expr st e
+			
 transOper :: M_operation -> M_expr -> ST -> I_opn
 transOper op e st = case op of
 	M_fn str ->  ICALL	(label, lvl)
 		where 
 			(I_FUNCTION (lvl, label, _, _)) = look_up st str
 	M_add    -> (case e of
-		M_ival v -> IADD_I
+		M_ival v -> IADD
 		M_rval v -> IADD_F
+		M_size v -> IADD
 		M_id v -> (case (transId e st) of
-			M_int -> IMUL_I
-			M_real -> IMUL_F)
-		x -> error ((show op )++ (show e)++(show st)))
+			M_int -> IADD
+			M_real -> IADD_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IADD
+			M_real -> IADD_F)
+		_ -> error ((show op )++ (show e)++(show st)))
 	M_mul    -> (case e of
-		M_ival v -> IMUL_I
+		M_ival v -> IMUL
 		M_rval v -> IMUL_F
-		M_id v -> IMUL_F		
-		x -> error ((show op )++ (show e)++(show st)))
-	M_sub    -> ISUB_F
-	M_div    -> IDIV_F
-	M_neg    -> INEG
-	M_lt     -> ILT
-	M_le     -> ILE
-	M_gt     -> IGT
-	M_ge     -> IGE
-	M_eq     -> IEQ
+		M_size v -> IMUL
+		M_id v -> (case (transId e st) of
+			M_int -> IMUL
+			M_real -> IMUL_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IMUL
+			M_real -> IMUL_F)	
+		_ -> error ((show op )++ (show e)++(show st)))
+	M_sub    -> (case e of 
+		M_ival v -> ISUB
+		M_rval v -> ISUB_F
+		M_size v -> ISUB
+		M_id v -> (case (transId e st) of
+			M_int -> ISUB
+			M_real -> ISUB_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> ISUB
+			M_real -> ISUB_F))	
+	M_div    -> (case e of 
+		M_ival v -> IDIV
+		M_rval v -> IDIV_F
+		M_size v -> IDIV
+		M_id v -> (case (transId e st) of
+			M_int -> IDIV
+			M_real -> IDIV_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IDIV
+			M_real -> IDIV_F))
+	M_neg    -> (case e of 
+		M_ival v -> INEG
+		M_rval v -> INEG_F
+		M_size v -> INEG
+		M_id v -> (case (transId e st) of
+			M_int -> INEG
+			M_real -> INEG_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> INEG
+			M_real -> INEG_F))
+	M_lt     -> (case e of 
+		M_ival v -> ILT
+		M_rval v -> ILT_F
+		M_size v -> ILT
+		M_id v -> (case (transId e st) of
+			M_int -> ILT
+			M_real -> ILT_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> ILT
+			M_real -> ILT_F))
+	M_le     -> (case e of 
+		M_ival v -> ILE
+		M_rval v -> ILE_F
+		M_size v -> ILE
+		M_id v -> (case (transId e st) of
+			M_int -> ILE
+			M_real -> ILE_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> ILE
+			M_real -> ILE_F))
+	M_gt     -> (case e of 
+		M_ival v -> IGT
+		M_rval v -> IGT_F
+		M_size v -> IGT
+		M_id v -> (case (transId e st) of
+			M_int -> IGT
+			M_real -> IGT_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IGT
+			M_real -> IGT_F))
+	M_ge     -> (case e of 
+		M_ival v -> IGE
+		M_rval v -> IGE_F
+		M_size v -> IGE
+		M_id v -> (case (transId e st) of
+			M_int -> IGE
+			M_real -> IGE_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IGE
+			M_real -> IGE_F))
+	M_eq     -> (case e of 
+		M_ival v -> IEQ
+		M_rval v -> IEQ_F
+		M_size v -> IEQ
+		M_id v -> (case (transId e st) of
+			M_int -> IEQ
+			M_real -> IEQ_F)
+		M_app (op, e:exs) -> (case (get_type_expr st e) of
+			M_int -> IEQ
+			M_real -> IEQ_F))
 	M_not    -> INOT
 	M_and    -> IAND
 	M_or     -> IOR
